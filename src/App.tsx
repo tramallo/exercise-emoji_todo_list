ec76572c14ab914f98ca5355bb53779e45c62016
tramallo <timo.22394@hotmail.com>
2024-09-15 22:17:21 -0300

    TodosList -> add theme support || App -> add theme switch func | add light & dark themes sample

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,16 @@ import TodosList from "./components/TodosList";
 import { Todo } from "./utils/schemas";
 
 export default function App() {
+  const [ currentTheme, setCurrentTheme ] = useState("light-theme")
   const [ todos, setTodos ] = useState<Todo[]>([])
+
+  const switchTheme = () => {
+    if (currentTheme == "light-theme") {
+      setCurrentTheme("dark-theme")
+    } else {
+      setCurrentTheme("light-theme")
+    }
+  }
 
   const addTodo = (newTodo: Todo) => {
     setTodos([...todos, newTodo])
@@ -20,13 +29,15 @@ export default function App() {
   }
 
   return (
-    <div className="app">
+    <div className={`app ${currentTheme}`}>
       <TodosList
         className="custom-todos-list"
         todos={todos}
         onTodoSelect={removeTodo}
       />
       <NewTodo onNewTodo={addTodo} options={{defaultEmoji: "♦"}} />
+
+      <button onClick={switchTheme}>Switch theme</button>
     </div>
   )
 }
