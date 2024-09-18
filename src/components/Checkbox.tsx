a03826cd04d097f978445358bdd43455f93d2533
tramallo <timo.22394@hotmail.com>
2024-09-17 23:13:00 -0300

    Checkbox -> add state change on enter press | fix hover & focus events styling

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -8,30 +8,52 @@ export type CheckboxProps = {
   labelChecked?: string;
   defaultState?: boolean;
   onStateChange?: (newState: boolean) => void;
-}
+};
 
-export default function Checkbox({ className, label, labelChecked, defaultState, onStateChange }: CheckboxProps) {
-  const [ checked, setChecked ] = useState<boolean>(!!defaultState);
+export default function Checkbox({
+  className,
+  label,
+  labelChecked,
+  defaultState,
+  onStateChange,
+}: CheckboxProps) {
+  const [checked, setChecked] = useState<boolean>(!!defaultState);
 
+  //send callback when state changes & updateLocalState
   const handleStateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const checkbox = e.target as HTMLInputElement
+    const checkbox = e.target as HTMLInputElement;
 
-    setChecked(checkbox.checked)
+    setChecked(checkbox.checked);
 
     if (onStateChange) {
-      onStateChange(checkbox.checked)
+      onStateChange(checkbox.checked);
     }
-  }
+  };
+
+  //change state when 'enter' is pressed on label
+  const handleEnter = (e: React.KeyboardEvent<HTMLLabelElement>) => {
+    if (e.key != "Enter") {
+      return;
+    }
+
+    //use click() to fire the default ChangeEvent
+    const label = e.target as HTMLLabelElement;
+    label.click();
+  };
 
   return (
-    <label className={`checkbox ${className ?? ""}`} tabIndex={0}>
-      <input 
-        type="checkbox" 
-        checked={checked} 
-        onChange={handleStateChange} 
+    <label
+      className={`checkbox ${className ?? ""}`}
+      tabIndex={0}
+      onKeyDown={handleEnter}
+    >
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={handleStateChange}
         tabIndex={-1}
       />
-      { checked ? (labelChecked ?? label) : label }
+      {checked ? labelChecked ?? label : label}
     </label>
-  )
+  );
 }
